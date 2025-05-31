ef94a95def9b801bcb76405a0a7ff01b77f222b3
randeepsingh48 <156921899+randeepsingh48@users.noreply.github.com>
2025-05-31 21:09:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'youlinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'ef986d167a323ae87811f3be0830dd065d0a6f25')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arlinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '324675b98650a9ec099b3e67ce2b1c1b02d8a95c')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
